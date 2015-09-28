e086b4a955c50f7665f3d9429d79618b80883be5
sylvain lefebvre <slvn.lefebvre@gmail.com>
2015-09-28 11:13:18 +0200

    Pave

---
+++ src/Pave.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+
+public class Pave {
+    public static void main(String ... args) {
+
+	//1. Demander chaque longueur grâce au Scanner
+	Scanner scan = new Scanner(System.in);
+	
+    }
+}
